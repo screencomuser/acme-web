3b74fc0c9f8d839abd4444ff44ec33a0330ac4da
ScreenCom User <admin@screencom.eu>
2022-01-20 16:40:37 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+
 cd $(dirname $0)
-python -m SimpleHTTPServer 63443
+
+if [[ $(which python3) ]]; then
+  python3 -m http.server 63443
+else
+  python -m SimpleHTTPServer 63443
+fi
